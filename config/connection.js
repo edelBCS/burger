03ff0a8b8e0ac50ee068ec108ea69199a11d3c0c
edelBCS <edelm8@gmail.com>
2019-09-26 10:59:32 -0700

    moved dotenv into jawsdb connditional in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,4 @@
-require("dotenv").config();
-var dbAuth = require("./dbAuth.js");
+
 
 var mysql = require("mysql");
 
@@ -8,17 +7,18 @@ var connection;
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
+    require("dotenv").config();
+    var dbAuth = require("./dbAuth.js");
     connection= mysql.createConnection(dbAuth.mysqlAuth);
 }
   
-  connection.connect(function(err) {
+connection.connect(function(err) {
     if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
+        console.error("error connecting: " + err.stack);
+        return;
     }
-  
     console.log("connected as id " + connection.threadId);
-  });
+});
 
-  module.exports = connection;
+module.exports = connection;
   
