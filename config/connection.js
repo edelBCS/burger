da494ba35d33663ca280754fdbc1292e09b06e9b
edelBCS <edelm8@gmail.com>
2019-09-26 10:20:39 -0700

    added heroku JAWSDB and .env for db Auth

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
+require("dotenv").config();
+var dbAuth = require("./dbAuth.js");
+
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Emar1956",
-    database: "burgers_db"
-  });
+var connection;
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection= mysql.createConnection(dbAuth.mysqlAuth);
+}
   
   connection.connect(function(err) {
     if (err) {
---
+++ config/dbAuth.js
@@ -0,0 +1,7 @@
+exports.mysqlAuth = {
+    host: process.env.mysqlHOST,
+    port: process.env.mysqlPORT,
+    user: process.env.mysqlUSER,
+    password: process.env.mysqlPASSWD,
+    database: process.env.mysqlDB
+};
